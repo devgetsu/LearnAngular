d69f63bdd07610d782591dfa5670c4235ff56fab
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-04-07 16:25:20 +0500

    add routerlink

--- leet-qode/src/app/nobbar/nobbar.component.ts
+++ leet-qode/src/app/nobbar/nobbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-nobbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './nobbar.component.html',
   styleUrl: './nobbar.component.scss'
 })
