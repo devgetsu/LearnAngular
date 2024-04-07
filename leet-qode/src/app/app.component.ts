25dab9bd6e37273ca0355700c0b70040d00079bc
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-04-07 07:48:15 +0500

    add main table

--- leet-qode/src/app/app.component.ts
+++ leet-qode/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NobbarComponent } from './nobbar/nobbar.component';
-import { HomebarComponent } from './homebar/homebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NobbarComponent,HomebarComponent],
+  imports: [RouterOutlet,NobbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- leet-qode/src/app/app.routes.ts
+++ leet-qode/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './component/profile/profile.component';
+import { HomebarComponent } from './homebar/homebar.component';
 
 export const routes: Routes = [
-    {path: 'profile', component : ProfileComponent}
+    {path:'' , component : HomebarComponent},
+    {path: 'profile', component : ProfileComponent},
+    {path: 'home', component : HomebarComponent},
+    {path:'**' , component : HomebarComponent}
 ];
